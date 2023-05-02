2c0d523f73291d6d9ef1013f3393fd2f8eb2ec80
your_username <your_email@gmail.com>
2023-05-02 23:14:57 +1000

    add new sample product

--- products/main.py
+++ products/main.py
@@ -1,9 +1,23 @@
+from datetime import datetime
+
 from products import Product
 
 
 def main():
-    print("in main ")
-    p = Product()
+    today = datetime.today().strftime("%Y-%m-%d")
+
+    # ?: why is everything a string. No native type compatibility?
+    # Tuple is preferrable as opposed to dictionary - as DB al
+    product = (
+        today,
+        "T- Shirts",
+        "Awesome Shirts",
+        "Custom logo",
+        "24.99",
+        "http://www.shop.com/products/custom-logo.html",
+    )
+    db = Product()
+    db.insert(product)
 
 
 if __name__ == "__main__":
