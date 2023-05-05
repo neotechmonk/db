c8bf853f1107899ef3f9c4dc53395ccef9e304ad
your_username <your_email@gmail.com>
2023-05-05 11:05:03 +1000

    refactored creating persons into add_persons()

--- sqlalchemy/main.py
+++ sqlalchemy/main.py
@@ -3,11 +3,16 @@ from models import Person
 
 session = Session()
 
-person1 = Person("Mike", "John", "m", 23)
-person2 = Person("Jane", "Doe", "f", 35)
-person3 = Person("Bob", "Smith", "m", 45)
-person4 = Person("Alice", "Jones", "f", 27)
-person5 = Person("Tom", "Brown", "m", 31)
+def add_persons():
+    person1 = Person("Mike", "John", "m", 23)
+    person2 = Person("Jane", "Doe", "f", 35)
+    person3 = Person("Bob", "Smith", "m", 45)
+    person4 = Person("Alice", "Jones", "f", 27)
+    person5 = Person("Tom", "Brown", "m", 31)
 
-session.add_all([person1, person2, person3, person4, person5])
-session.commit()
+    session.add_all([person1, person2, person3, person4, person5])
+    session.commit()
+
+
+if __name__ == "__main__":
+    add_persons()
