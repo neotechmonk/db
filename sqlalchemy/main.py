c6b750461e73bad9b33bfc6ed4226b8947efcf57
your_username <your_email@gmail.com>
2023-05-05 11:55:00 +1000

    find people by their last name

--- sqlalchemy/main.py
+++ sqlalchemy/main.py
@@ -8,18 +8,29 @@ def add_persons(session):
     person1 = Person(123, "Mike", "John", "m", 23)
     person2 = Person(124,"Jane", "Doe", "f", 35)
     person3 = Person(1234,"Bob", "Smith", "m", 45)
-    person4 = Person(2134,"Alice", "Jones", "f", 27)
+    person4 = Person(2134,"Alice", "Smith", "f", 27)
     person5 = Person(3451,"Tom", "Brown", "m", 31)
 
     session.add_all([person1, person2, person3, person4, person5])
     session.commit()
 
 def get_all_persons(session):
-    results = session.query(Person).all()
-    return results
+    return session.query(Person).all()
+
+
+
+def findby_name(session, search):
+    """
+    Ad
+    """
+    return session.query(Person).filter(Person.lastname == search).all()
+
 
 if __name__ == "__main__":
     session = Session()
-    add_persons(session)
-    results = get_all_persons(session)
-    pprint(results)
+    # add_persons(session)
+    # pprint(get_all_persons(session))
+
+    # Search people by the last name
+    for x in findby_name(session, 'Smith'): 
+        pprint(x)
